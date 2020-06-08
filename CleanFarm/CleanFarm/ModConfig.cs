ad76cebc9ee6375736abaa1464c1658385b373e1
Fagara <66609385+Fagara@users.noreply.github.com>
2020-06-08 15:28:52 +0700

    Update ModConfig.cs

--- CleanFarm/CleanFarm/ModConfig.cs
+++ CleanFarm/CleanFarm/ModConfig.cs
@@ -5,22 +5,22 @@ namespace CleanFarm
     public class ModConfig
     {
         /// <summary>Should we remove all the grass.</summary>
-        public bool RemoveGrass { get; set; } = false;
+        public bool RemoveGrass { get; set; } = true;
 
         /// <summary>Should we remove all the weeds.</summary>
         public bool RemoveWeeds { get; set; } = true;
 
         /// <summary>Should we remove all the stones.</summary>
-        public bool RemoveStones { get; set; } = false;
+        public bool RemoveStones { get; set; } = true;
 
         /// <summary>Should we remove all the twigs.</summary>
-        public bool RemoveTwigs { get; set; } = false;
+        public bool RemoveTwigs { get; set; } = true;
 
         /// <summary>Should we remove all tree stumps.</summary>
-        public bool RemoveStumps { get; set; } = false;
+        public bool RemoveStumps { get; set; } = true;
 
         /// <summary>Should we remove all saplings (all trees below growth stage 5).</summary>
-        public bool RemoveSaplings { get; set; } = false;
+        public bool RemoveSaplings { get; set; } = true;
 
         /// <summary>
         /// If RemoveSaplings is true then all trees with a max growth stage below this value will be removed.
@@ -29,10 +29,10 @@ namespace CleanFarm
         public int MaxTreeGrowthStageToAllow { get; set; } = 5;
 
         /// <summary>Should we remove all the large log resource clumps.</summary>
-        public bool RemoveLargeLogs { get; set; } = false;
+        public bool RemoveLargeLogs { get; set; } = true;
 
         /// <summary>Should we remove all large rock resource clumps.</summary>
-        public bool RemoveLargeRocks { get; set; } = false;
+        public bool RemoveLargeRocks { get; set; } = true;
 
         /// <summary>Should bushes be removed.</summary>
         public bool RemoveBushes { get; set; } = false;
